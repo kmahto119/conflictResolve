f047aa620fbfe1ecc92517e789abe7378d8e79e9
kamlesh <kamleshkec119@gmail.com>
2020-08-30 00:19:19 +0530

    commit

--- src/com/add/Hellow.java
+++ src/com/add/Hellow.java
@@ -4,11 +4,7 @@ public class Hellow {
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a+"===>"+",");
-<<<<<<< HEAD
-		System.out.println("hi================="+"kkk");
-=======
-		System.out.println("hi================="+"=====>>>>>>>>>>>>>>");
->>>>>>> ec7c9a4df4073e6a86eea5765aebb4f9f5d4a42d
+
 		System.out.println("hi================="+"=====>>>>>>>>>>>>>>");
 	}
 
