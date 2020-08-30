46e10492bd150991adb76391c3a09830b05e8b98
kamlesh <kamleshkec119@gmail.com>
2020-08-30 16:10:01 +0530

    "modify some statement delete some lines"

--- src/com/add/Hellow.java
+++ src/com/add/Hellow.java
@@ -4,9 +4,6 @@ public class Hellow {
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a+"===>"+",");
-		
-
-		System.out.println("hi================="+"=====>>>>>>>>>>>>>>");
 	}
 
 }
