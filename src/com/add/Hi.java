6141bb0c640e7d1d09ced20edc4a151e658ae0b3
kamlesh <kamleshkec119@gmail.com>
2020-08-30 16:38:05 +0530

    first

--- src/com/add/Hi.java
+++ src/com/add/Hi.java
@@ -3,7 +3,7 @@ package com.add;
 public class Hi {
 	public static int m=90;
 	public static void main(String[] args) {
-		System.out.println("value=="+m);
+		System.out.println("value="+m);
 		
 	}
 
