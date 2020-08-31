32ea7db05da2bd72253e9d2cf3c27c8322ccc8fb
kamlesh <kamleshkec119@gmail.com>
2020-08-31 07:37:32 +0530

    hi page commit 1st

--- src/com/add/Hi.java
+++ src/com/add/Hi.java
@@ -3,7 +3,7 @@ package com.add;
 public class Hi {
 	public static int m=90;
 	public static void main(String[] args) {
-		System.out.println("value="+m+"==>"+m);
+		System.out.println(m+"=====>"+"my name is kamlesh");
 	}
 
 }
