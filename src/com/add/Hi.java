609c052f8970352ab1f40fa545beaa0c7c603e9f
kamlesh <kamleshkec119@gmail.com>
2020-08-31 09:14:29 +0530

    8th try

--- src/com/add/Hi.java
+++ src/com/add/Hi.java
@@ -3,10 +3,7 @@ package com.add;
 public class Hi {
 	public static int m=90;
 	public static void main(String[] args) {
-
-
-
-		System.out.println(m+"=====>"+"my name is kamlesh"+"jamshedpur");
+		System.out.println(m + "=====>"+"jamshedpur");
 
 
 	}
