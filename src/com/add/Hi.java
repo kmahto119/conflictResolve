5449168969250343084458f09afa3f045c539b79
kamlesh <kamleshkec119@gmail.com>
2020-08-31 07:42:50 +0530

    2nd commit

--- src/com/add/Hi.java
+++ src/com/add/Hi.java
@@ -3,7 +3,7 @@ package com.add;
 public class Hi {
 	public static int m=90;
 	public static void main(String[] args) {
-		System.out.println("value="+m+"==>"+m);
+		System.out.println("value="+m+"my name is kamlesh"+ "i am from jamshedpur");
 	}
 
 }
